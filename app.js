bb240ad3ba54ebecb87c3407db47789b54d547c0
Mark Wallace <mark_wallace@ie.ibm.com>
2018-05-21 21:24:07 +0100

    Send targeted message which displays an action fulfillment card

--- app.js
+++ app.js
@@ -1,10 +1,102 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import crypto from 'crypto';
+import request from 'request';
 
 const appId = process.env.CLIENT_ID;
 const appSecret = process.env.CLIENT_SECRET;
 const webhookSecret = process.env.WEBHOOK_SECRET;
+
+const watsonworkUrl = 'https://api.watsonwork.ibm.com';
+
+const createTargetedMessage = (conversationId, targetUserId, targetDialogId) => {
+  //return `mutation {createTargetedMessage(input: {conversationId: "${conversationId}", targetUserId: "${targetUserId}", targetDialogId: "${targetDialogId}", attachments: [{type: CARD, cardInput: {type: INFORMATION, informationCardInput: {title: "File it?", subtitle: "tests", text: "test message", date: "1526046506357", buttons: [{text: "Yes", payload: "yes", style: PRIMARY}]}}}]}) {successful}}`;
+  return `mutation {
+    createTargetedMessage(input: {
+      conversationId: "${conversationId}",
+      targetUserId: "${targetUserId}",
+      targetDialogId: "${targetDialogId}",
+      attachments: [
+        {
+          type: CARD,
+          cardInput: {
+            type: INFORMATION,
+            informationCardInput: {
+              title: "Card Title",
+              subtitle: "Card Subtitle",
+              text: "Card Text",
+              date: "1526046506357",
+              buttons: [
+                {
+                  text: "Button Text",
+                  payload: "Button Payload",
+                  style: PRIMARY
+                }
+              ]
+            }
+          }
+        }
+      ]
+    }) {
+      successful
+    }
+  }`;
+}
+
+const authenticateApp = (callback) => {
+  const authenticationOptions = {
+    'method': 'POST',
+    'url': 'https://api.watsonwork.ibm.com/oauth/token',
+    'auth': {
+      'user': appId,
+      'pass': appSecret
+    },
+    'form': {
+      'grant_type': 'client_credentials'
+    }
+  };
+
+  request(authenticationOptions, (err, response, body) => {
+    if (response.statusCode != 200) {
+      console.log('Error authentication application. Exiting.');
+      process.exit(1);
+    }
+    callback(JSON.parse(body).access_token);
+  });
+};
+
+const sendMessage = (req) => {
+  const annotationPayload = JSON.parse(req.body.annotationPayload);
+  const conversationId = annotationPayload.conversationId;
+  const targetDialogId = annotationPayload.targetDialogId;
+  const targetUserId = req.body.userId;
+
+  const messageData = createTargetedMessage(conversationId, targetUserId, targetDialogId);
+  console.log('TARGETED MESSAGE: ' + JSON.stringify(messageData));
+
+  authenticateApp( (jwt) => {
+
+    const sendMessageOptions = {
+      'method': 'POST',
+      'url': 'https://api.watsonwork.ibm.com/graphql',
+      'headers': {
+        'Authorization': `Bearer ${jwt}`,
+        'Content-Type': 'application/graphql',
+        'x-graphql-view': 'PUBLIC, BETA'
+      },
+      'body': messageData
+    };
+
+    request(sendMessageOptions, (err, response, body) => {
+      if(response.statusCode != 200) {
+        console.log('Error creating targeted message.');
+        console.log(response.statusCode);
+        console.log(err);
+        console.log(body);
+      }
+    });
+  });
+};
 
 const respondOk = (res) => {
   res.status(200).end();
@@ -21,7 +113,6 @@ const handleVerificationRequest = (req, res) => {
     crypto.createHmac('sha256', webhookSecret)
     .update(JSON.stringify(bodyToSend))
     .digest('hex');
-  console.log('X-OUTBOUND-TOKEN' + hashToSend);
 
   res.set('X-OUTBOUND-TOKEN', hashToSend);
   res.send(bodyToSend).end();
@@ -30,9 +121,10 @@ const handleVerificationRequest = (req, res) => {
 const validateRequest = (req, res, next) => {
   const processRequest = {
     'verification': handleVerificationRequest,
-    'message-created': () => next()
+    'message-annotation-added': () => next()
   };
 
+  console.log('TYPE: ' + JSON.stringify(req.body.type));
   return (processRequest[req.body.type]) ?
     processRequest[req.body.type](req, res) : respondOk(res);
 };
@@ -47,7 +139,10 @@ app.get('/', (req, res) => {
 
 app.post('/webhook', validateRequest, (req, res) => {
   console.log('BODY: ' + JSON.stringify(req.body));
+
   respondOk(res);
+
+  sendMessage(req);
 });
 
 var port = process.env.PORT || 3000
